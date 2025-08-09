468e3fd4089c6a9c5ab5dc0514cfac0592c7d568
Captain-Sadistic <sadmanmorolishmam@gmail.com>
2025-08-09 17:17:02 +0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+     vite: {
+    server: {
+      host: true, // allow external connections
+      allowedHosts: ['devserver-preview--captainsadistic.netlify.app']
+    },
+    preview: {
+      host: true,
+      allowedHosts: ['devserver-preview--captainsadistic.netlify.app']
     vite: {
         plugins: [tailwindcss()]
     },
